fd6aaf251cd1d0f08eee80b03a5e4b90929e89be
jarokaz <jarokaz>
2019-10-19 23:51:56 +0000

    Updates

--- infrastructure/ml-pipeline-environment/terraform/backend.tf
+++ infrastructure/ml-pipeline-environment/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
  backend "gcs" {
    bucket  = "jkterraform"
-   prefix  = "terraform/state/mlops/caip-tfx/dev-env"
+   prefix  = "terraform/state/mlops/caip-tfx/dev-env5"
  }
 }
--- infrastructure/ml-pipeline-environment/terraform/terraform.tfvars
+++ infrastructure/ml-pipeline-environment/terraform/terraform.tfvars
@@ -1,4 +1,4 @@
-project_id   = "jk-demo1"
+project_id   = "jk-demo2"
 region       = "us-central1"
 zone         = "us-central1-a"
-name_prefix  = "tfx-dev2"
+name_prefix  = "tfx-dev5"
